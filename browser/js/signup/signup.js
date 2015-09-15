1c618f9546d8924f5bba697d69a73db766abbdcd
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-09-15 17:29:56 -0400

    local login

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -3,7 +3,29 @@ app.config(function ($stateProvider) {
     $stateProvider.state('signup', {
         url: '/signup',
         templateUrl: 'js/signup/signup.html',
-        //controller: 'LoginCtrl'
+        controller: 'SignupCtrl'
     });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+    $scope.login = {};
+    $scope.error = null;
+
+    $scope.sendLogin = function (loginInfo) {
+        console.log('send signup');
+        $scope.error = null;
+
+        AuthService.signup(loginInfo).then(function(){
+          AuthService.login(loginInfo).then(function () {
+              $state.go('home');
+          }).catch(function () {
+              $scope.error = 'Invalid login credentials.';
+          });
+        }).then(null,function(err){
+          $scope.error=err;
+        });
+    };
 
 });
