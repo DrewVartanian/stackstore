de28c808bee5121fc3f27a4e4e6505f4fc26be87
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-09-17 11:24:09 -0400

    added member access verification through members rout

--- server/app/api/members/orders/index.js
+++ server/app/api/members/orders/index.js
@@ -15,11 +15,9 @@ var Order = mongoose.model('Order');
 // };
 
 router.get('/', function (req,res,next){
-    console.log("this is going to show orders when its done");
     //res.json({orderId: 123456});
 
     Order.find({user: req.userParam._id}).populate('items.productId').exec().then(function(orders){
-        console.log("Orders: ",orders);
         res.status(200).json(orders);
     }).then(null, next);
 
