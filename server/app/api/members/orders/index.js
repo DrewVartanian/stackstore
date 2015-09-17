121199c2568f7044f6854d985778b16c6812144b
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-09-17 11:49:31 -0400

    differenciate between cart and order history and display/route them independently

--- server/app/api/members/orders/index.js
+++ server/app/api/members/orders/index.js
@@ -17,8 +17,16 @@ var Order = mongoose.model('Order');
 router.get('/', function (req,res,next){
     //res.json({orderId: 123456});
 
-    Order.find({user: req.userParam._id}).populate('items.productId').exec().then(function(orders){
+    Order.find({user: req.userParam._id,date:{$ne:null}}).populate('items.productId').exec().then(function(orders){
         res.status(200).json(orders);
+    }).then(null, next);
+
+});
+
+router.get('/cart', function (req,res,next){
+
+    Order.findOne({user: req.userParam._id,date:null}).populate('items.productId').exec().then(function(cart){
+        res.status(200).json(cart);
     }).then(null, next);
 
 });
