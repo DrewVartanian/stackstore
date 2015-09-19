da459c87810a9e029751df2e32319736fd33db15
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-09-19 18:09:14 -0400

    Product Edit

--- server/app/api/products/index.js
+++ server/app/api/products/index.js
@@ -12,11 +12,43 @@ router.get('/', function(req, res, next) { //next here
 
 });
 
+router.param('id', function (req,res,next,id){
+  Product.findById(id).then(function(product) {
+      req.productParm=product;
+      next();
+  }).then(null,function(err){
+    err.status = 404;
+    throw err;
+  }).then(null,next);
+});
+
 router.get('/:id', function(req, res, next) { //next here
+    res.json(req.productParm);
+});
 
-    Product.findById(req.params.id).then(function(product) {
-        res.json(product);
-    })
-    .then(null, next);
+router.delete('/:id', function(req, res, next) { //next here
+    if(!req.user.isAdmin){
+      var err = new Error('Admins Only');
+      err.status = 403;
+      return next(err);
+    }
+    req.productParm.remove().then(function() {
+        //check status code
+        res.sendStatus(204);
+    }).then(null, next);
+});
 
+router.put('/:id', function(req, res, next) { //next here
+    if(!req.user.isAdmin){
+      var err = new Error('Admins Only');
+      err.status = 403;
+      return next(err);
+    }
+    for(var key in req.body){
+      req.productParm[key]=req.body[key];
+    }
+    req.productParm.save().then(function(product) {
+        //check status code
+        res.status(product).json(product);
+    }).then(null, next);
 });
